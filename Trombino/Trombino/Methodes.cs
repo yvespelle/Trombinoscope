82b56487c39e110765510412475f2ad3a233ee66
yvespelle <40024398+yvespelle@users.noreply.github.com>
2018-07-25 14:36:41 +0200

    Add files via upload

---
+++ Trombino/Trombino/Methodes.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Trombino
+{
+    public partial class Form1
+    {
+        PictureBox[] box;
+        int i;
+        int j = 1;
+        popup popup;
+
+
+        private void connexion()
+        {
+            string connetionString = null;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(connetionString);
+
+            try
+            {
+                cnn.Open();
+                //label1.Text = "Connexion établie";
+                SqlCommand requete = cnn.CreateCommand();
+                requete.CommandText = "SELECT * FROM IDENTIFIANTS";
+                SqlDataReader dataReader = requete.ExecuteReader();
+
+
+                string[] tab = new string[dataReader.FieldCount];
+
+
+                while (dataReader.Read())
+                {
+
+                }
+
+
+
+                dataReader.Close();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Echec de connexion ! ");
+            }
+        }
+
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+        // ------------------------------------------- Methode afficher les photos -----------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        private void AfficherLesImages()
+        {
+
+            // connexion à la base
+            string connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
+            SqlConnection sqlConn = new SqlConnection(connetionString);
+
+            List<ImageList> list = new List<ImageList>();
+            string dept;
+            SqlCommand cmd;
+            //string val = comboBox1.Items.
+            if (comboBox1.Text == comboBox1.DisplayMember)
+            {
+                string query = String.Format(@"SELECT PHOTO FROM IDENTIFIANTS"); // on ne recupère que l'image
+                cmd = new SqlCommand(query, sqlConn);
+            }
+            else
+            {
+                dept = (comboBox1.SelectedItem as dynamic).Value; // récupère la valeur correspondante au texte entré dans la combobox
+                string query = String.Format(@"SELECT PHOTO FROM IDENTIFIANTS WHERE DEPARTEMENT= @dept"); // on ne recupère que l'image
+                cmd = new SqlCommand(query, sqlConn);
+                cmd.Parameters.AddWithValue("@dept", dept);
+
+            }
+
+            cmd.Connection.Open();
+
+            ConstructionBox();
+            ViderBox();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            j = 1;
+            while (reader.Read())
+            {
+
+                ImageList _image = new ImageList();
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    byte[] data = (byte[])reader[i];
+                    MemoryStream ms = new MemoryStream(data);
+                    Image image = new Bitmap(ms); // on converti en Bitmap pour récupérer une image
+                    box[j].Image = image; // affichage de l'image dans la PictureBox
+                    list.Add(_image);
+                    j++;
+                }
+            }
+
+            reader.Close();
+            sqlConn.Close();
+
+            if (list.Count == 0)
+                label1.Text = "Il n'y a pas de photo correspondante à la recherche.";
+
+
+        }
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+        // -----------------------------------Methode afficher détails à partir d'une photo---------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+
+        private void AfficherDetails(int p)
+        {
+            popup = new popup();
+            AfficherLeNom(p);
+            AfficherPhotoPopUp(p);
+            AfficherLesCoordonnees(p);
+            AfficherLesClients(p);
+        }
+
+
+
+
+
+
+        private void AfficherLeNom(int p)
+        {
+            string connetionString = null;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(connetionString);
+
+            if (box[p].Image != null) // empeche d'avoir une erreur lorsqu'on clic sur unr photo "vide"
+            {
+                byte[] code = imageToByteArray(box[p].Image);
+                cnn.Open();
+
+                SqlCommand requete = cnn.CreateCommand();
+                requete.CommandText = "SELECT NOM, PRENOM FROM IDENTIFIANTS WHERE PHOTO=@PHOTO";
+                requete.Parameters.Add(new SqlParameter("@PHOTO", code)); // récupération du parametre à afficher
+
+                SqlDataReader dataReader = requete.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+
+                    for (int i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        string[] tab = new string[dataReader.FieldCount];
+                        tab[i] = dataReader.GetValue(i).ToString(); //récupération du résultat dans un tableau avant de le retourner
+                        popup.textBoxPopup1.Text = popup.textBoxPopup1.Text + tab[i] + Environment.NewLine + Environment.NewLine;
+                        popup.Show();
+
+                    }
+                }
+
+                dataReader.Close();
+                cnn.Close();
+            }
+        }
+
+
+
+
+
+        private void AfficherLesCoordonnees(int p)
+        {
+            string connetionString = null;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(connetionString);
+
+            if (box[p].Image != null) // empeche d'avoir une erreur lorsqu'on clic sur unr photo "vide"
+            {
+                byte[] code = imageToByteArray(box[p].Image);
+                cnn.Open();
+
+                SqlCommand requete = cnn.CreateCommand();
+                requete.CommandText = "SELECT TELEPHONE, MAIL FROM IDENTIFIANTS WHERE PHOTO=@PHOTO";
+                requete.Parameters.Add(new SqlParameter("@PHOTO", code)); // récupération du parametre à afficher
+
+                SqlDataReader dataReader = requete.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+
+                    for (int i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        string[] tab = new string[dataReader.FieldCount];
+                        tab[i] = dataReader.GetValue(i).ToString(); //récupération du résultat dans un tableau avant de le retourner
+                        popup.textBoxPopup2.Text = popup.textBoxPopup2.Text + tab[i] + Environment.NewLine;
+                        popup.Show();
+
+                    }
+                }
+
+                dataReader.Close();
+                cnn.Close();
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void AfficherLesClients(int p)
+        {
+            string connetionString = null;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(connetionString);
+
+            if (box[p].Image != null) // empeche d'avoir une erreur lorsqu'on clic sur unr photo "vide"
+            {
+                byte[] code = imageToByteArray(box[p].Image);
+                cnn.Open();
+
+                SqlCommand requete = cnn.CreateCommand();
+                requete.CommandText = "SELECT CLIENTS, COLLABORATEURS FROM IDENTIFIANTS WHERE PHOTO=@PHOTO";
+                requete.Parameters.Add(new SqlParameter("@PHOTO", code)); // récupération du parametre à afficher
+
+                SqlDataReader dataReader = requete.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+
+                    for (int i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        string[] tab = new string[dataReader.FieldCount];
+                        tab[i] = dataReader.GetValue(i).ToString(); //récupération du résultat dans un tableau avant de le retourner
+                        popup.textBoxPopup3.Text = popup.textBoxPopup3.Text + tab[i] + Environment.NewLine + Environment.NewLine;
+                        popup.Show();
+
+                    }
+                }
+
+                dataReader.Close();
+                cnn.Close();
+            }
+        }
+
+
+
+
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+        // ------------------------------------------- Methode afficher la photo dans une popup ----------------------------------------------------------
+        // -----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        private void AfficherPhotoPopUp(int p)
+        {
+            string connetionString = null;
+            connetionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=test;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(connetionString);
+
+            if (box[p].Image != null)
+            {  // empeche d'avoir une erreur lorsqu'on clic sur unr photo "vide"
+                byte[] code = imageToByteArray(box[p].Image);
+                cnn.Open();
+                //label1.Text = "Connexion établie";
+
+                SqlCommand requete = cnn.CreateCommand();
+                requete.CommandText = "SELECT PHOTO FROM IDENTIFIANTS WHERE PHOTO=@PHOTO";
+                requete.Parameters.Add(new SqlParameter("@PHOTO", code)); // récupération du parametre à afficher
+
+                SqlDataReader reader = requete.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+                    byte[] data = (byte[])reader[i];
+                    MemoryStream ms = new MemoryStream(data);
+                    Image image = new Bitmap(ms); // on converti en Bitmap pour récupérer une image
+                    popup.pictureBox1.Image = image; // affichage de l'image dans la PictureBox
+                }
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+        // conversion données brute en photo 
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+        }
+
+
+        // efface les photos non utilisées 
+        public void ViderBox()
+        {
+            foreach (PictureBox pb in box)
+            {
+                if (pb != null)
+                {
+                    pb.Image = null;
+                }
+            }
+        }
+
+        public void ConstructionBox()
+        {
+            box = new PictureBox[50];
+
+            box[1] = pictureBox1;
+            box[2] = pictureBox2;
+            box[3] = pictureBox3;
+            box[4] = pictureBox4;
+            box[5] = pictureBox5;
+            box[6] = pictureBox6;
+            box[7] = pictureBox7;
+            box[8] = pictureBox8;
+            box[9] = pictureBox9;
+            box[10] = pictureBox10;
+            box[11] = pictureBox11;
+            box[12] = pictureBox12;
+            box[13] = pictureBox13;
+            box[14] = pictureBox14;
+            box[15] = pictureBox15;
+            box[16] = pictureBox16;
+            box[17] = pictureBox17;
+            box[18] = pictureBox18;
+            box[19] = pictureBox19;
+            box[20] = pictureBox20;
+            box[21] = pictureBox21;
+            box[22] = pictureBox22;
+            box[23] = pictureBox23;
+            box[24] = pictureBox24;
+            box[25] = pictureBox25;
+            box[26] = pictureBox26;
+            box[27] = pictureBox27;
+            box[28] = pictureBox28;
+            box[29] = pictureBox29;
+            box[30] = pictureBox30;
+            box[31] = pictureBox31;
+            box[32] = pictureBox32;
+            box[33] = pictureBox33;
+            box[34] = pictureBox34;
+            box[35] = pictureBox35;
+            box[36] = pictureBox36;
+            box[37] = pictureBox37;
+            box[38] = pictureBox38;
+            box[39] = pictureBox39;
+            box[40] = pictureBox40;
+            box[41] = pictureBox41;
+            box[42] = pictureBox42;
+            box[43] = pictureBox43;
+            box[44] = pictureBox44;
+            box[45] = pictureBox45;
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
---
+++ Trombino/Trombino/popup.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Trombino
+{
+    partial class popup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxPopup1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPopup2 = new System.Windows.Forms.TextBox();
+            this.textBoxPopup3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fermer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(127, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBoxPopup1
+            // 
+            this.textBoxPopup1.Location = new System.Drawing.Point(339, 108);
+            this.textBoxPopup1.Multiline = true;
+            this.textBoxPopup1.Name = "textBoxPopup1";
+            this.textBoxPopup1.Size = new System.Drawing.Size(224, 49);
+            this.textBoxPopup1.TabIndex = 2;
+            this.textBoxPopup1.TextChanged += new System.EventHandler(this.textBoxPopup1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(0, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(12260, 68);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Trombino.Properties.Resources.LOGO;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 7);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(126, 59);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 59);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10000, 10);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nom :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Prenom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Coordonnées :";
+            // 
+            // textBoxPopup2
+            // 
+            this.textBoxPopup2.Location = new System.Drawing.Point(371, 169);
+            this.textBoxPopup2.Multiline = true;
+            this.textBoxPopup2.Name = "textBoxPopup2";
+            this.textBoxPopup2.Size = new System.Drawing.Size(224, 49);
+            this.textBoxPopup2.TabIndex = 9;
+            // 
+            // textBoxPopup3
+            // 
+            this.textBoxPopup3.Location = new System.Drawing.Point(339, 233);
+            this.textBoxPopup3.Multiline = true;
+            this.textBoxPopup3.Name = "textBoxPopup3";
+            this.textBoxPopup3.Size = new System.Drawing.Size(224, 154);
+            this.textBoxPopup3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Clients :";
+            // 
+            // popup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPopup3);
+            this.Controls.Add(this.textBoxPopup2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPopup1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "popup";
+            this.Text = "Informations";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.PictureBox pictureBox1 { get; set; }
+        public System.Windows.Forms.TextBox textBoxPopup1 { get; set; }
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox textBoxPopup2;
+        public System.Windows.Forms.TextBox textBoxPopup3;
+        private System.Windows.Forms.Label label4;
+    }
+}
